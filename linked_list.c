d4b79167d9eb20bcd7e09adcc6a2a0f69fa7d741
Paul Slack <slack10@purdue.edu>
2025-04-03 02:45:29 +0000

    PC Work: April 2

--- linked_list.c
+++ linked_list.c
@@ -2,17 +2,76 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value) 
+{
+  struct list_node *node = malloc(sizeof(struct list_node));
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+  node -> value = value;
+  node -> next = NULL;
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+  return node; 
+}
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+void insert_at_head(struct linked_list *list, size_t value) 
+{
+  struct list_node* new_head = new_node(value);
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+  new_head -> next = list -> head;
+  list -> head = new_head;
+}
 
-void free_list(struct linked_list list) {}
+void insert_at_tail(struct linked_list *list, size_t value) 
+{
+  struct list_node* new_tail = new_node(value);
+  struct list_node* node = list -> head;
+
+  while (node -> next != NULL)
+  {
+    node = node -> next;
+  }
+
+  node -> next = new_tail;
+}
+
+size_t remove_from_head(struct linked_list *list) 
+{ 
+  size_t removed_value = list -> head -> value;
+
+  struct list_node* removed_head = list -> head;
+  list -> head = list -> head -> next;
+  free(removed_head);
+
+  return removed_value; 
+}
+
+size_t remove_from_tail(struct linked_list *list) 
+{ 
+  struct list_node* node = list -> head;
+
+  while (node -> next -> next != NULL)
+  {
+    node = node -> next;
+  }
+
+  struct list_node* removed_tail = node -> next;
+  size_t removed_value = removed_tail -> value;
+  node -> next = NULL;
+  free(removed_tail);
+
+  return removed_value; 
+}
+
+void free_list(struct linked_list list) 
+{
+  struct list_node* node = list.head;
+
+  while (node != NULL)
+  {
+    struct list_node* next_node = node -> next;
+    free(node);
+    node = next_node;
+  }
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
--- queue.c
+++ queue.c
@@ -1,8 +1,20 @@
 #include "queue.h"
 #include "tile_game.h"
 
-void enqueue(struct queue *q, struct game_state state) {}
+void enqueue(struct queue *q, struct game_state state) 
+{
+    struct linked_list list = q -> data;
+    insert_at_tail(&list, serialize(state));
+}
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+struct game_state dequeue(struct queue *q) 
+{ 
+    struct linked_list list = q -> data;
+    return deserialize(remove_from_head(&list));
+    // return (struct game_state){0}; 
+}
 
-int number_of_moves(struct game_state start) { return 0; }
+int number_of_moves(struct game_state start) 
+{ 
+    return 0; 
+}
