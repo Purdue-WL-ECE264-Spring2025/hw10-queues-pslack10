f06a688fd340072275a9f2a378ab7684c36d4ee0
Paul Slack <slack10@purdue.edu>
2025-04-03 12:56:01 -0400

    Laptop Work

--- queue.c
+++ queue.c
@@ -15,6 +15,17 @@ struct game_state dequeue(struct queue *q)
 }
 
 int number_of_moves(struct game_state start) 
-{ 
-    return 0; 
+{   
+    int finished_state[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 0}};  
+    struct queue q;
+    struct game_state current = start;
+    enqueue(&q, current);
+    
+    current = dequeue(&q);
+    if (current.tiles != finished_state)
+    {
+        enqueue()
+    }
+
+    return current.num_steps;
 }
